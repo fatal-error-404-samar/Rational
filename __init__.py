5098cdd8ba6f477c9191f174ad0d0d3c8d247d90
Samar <samarjyotipator78@gmail.com>
2025-06-03 18:48:55 +0530

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,12 @@
+"""
+rational: A Python library for rational number arithmetic.
+
+Author: Samar Jyoti Pator
+License: MIT
+"""
+
+from .rational import Rational
+
+__version__ = "1.0.0"
+__author__ = "Samar Jyoti Pator"
+__all__ = ["Rational"]
